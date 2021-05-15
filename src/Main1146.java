327f288fb9781214e2c722696b42bba8c9cb3b60
TiagoPBlaszcyk <tiagopcb@hotmail.com>
2021-05-14 22:26:04 -0300

    1.0.0

--- src/Main1146.java
+++ src/Main1146.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+
 public class Main1146 {
     public static void main(String[] args){
+
         Scanner s = new Scanner(System.in);
         int num;
         String exit = "";
+        
         num = s.nextInt();
         while (num != 0) {
             for (int i = 1; i <= num; i++) {
