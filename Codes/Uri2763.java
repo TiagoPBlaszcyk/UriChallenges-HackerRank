620417db82f49da6ce590e13b6155e19667afa70
TiagoPBlaszcyk <tiagopcb@hotmail.com>
2021-05-15 20:21:08 -0300

    2763 Desafiado Check

---
+++ Codes/Uri2763.java
@@ -0,0 +1,18 @@
+// CPF 320.025.102-01 filtrando somente numeros 
+import java.util.Scanner;
+import java.util.Locale;
+public class Uri2763{
+	public static void main(String[] args) {
+		Locale.setDefault(new Locale("en", "US"));
+		Scanner s = new Scanner(System.in);
+		String cpf = s.nextLine();
+        s.close();
+        for (int i = 1; i <= cpf.length(); i++) {
+            if (i % 4 != 0) {
+                System.out.print(cpf.charAt(i-1));
+            }else {
+                System.out.println();
+            }
+        }
+	}
+}
